b377e5bc013dfe5c509391dd7089a2d13c1085d3
Brandon Lewis <64657834+blewis308@users.noreply.github.com>
2021-03-31 09:57:28 -0500

    Update commands
    Added list, direct, and error

--- chatClient.java
+++ chatClient.java
@@ -11,9 +11,12 @@
         <1 byte> <2 byte> <length # of chars>
 
     Commands so far:
-    Join - <0> <len of name> <username>
-    Leave - <1> <null> <null>
-    Talk - <2> <len of message> <message>
+    Join -      <0> <len of name> <username>
+    Leave -     <1> <null> <null>
+    Talk -      <2> <len of message> <message>
+    List -      <3> 
+    Direct -    <4> 
+    Error -     <5> 
 
     Server will respond by sending this same format to all clients (including
     the sender) and will append the senders username to the beginning of <message>
