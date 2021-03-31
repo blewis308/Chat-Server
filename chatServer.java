dd0a2d294b2d94088a3c777b48ab5eef573445f7
Brandon Lewis <64657834+blewis308@users.noreply.github.com>
2021-03-31 09:52:23 -0500

    Updated with general comments
    General goal, command syntax, and basic list of commands so far.

--- chatServer.java
+++ chatServer.java
@@ -1,3 +1,26 @@
+/*
+
+    For more up-to-date info, check the Discord or your email.
+
+    General Goal:
+        To implement a chat server using an interoperable protocol.
+        Server runs on port 9000
+        
+    Commands will be formatted as follows:
+        <command num> <length> <message ascii>
+        <1 byte> <2 byte> <length # of chars>
+
+    Commands so far:
+    Join - <0> <len of name> <username>
+    Leave - <1> <null> <null>
+    Talk - <2> <len of message> <message>
+
+    Server will respond by sending this same format to all clients (including
+    the sender) and will append the senders username to the beginning of <message>
+
+    
+*/  
+
 public class chatServer {
 
 }
