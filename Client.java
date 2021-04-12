214fad1969381ddc346bdd4c6297dd069f10b965
Bailie Livingston <rampion@mindspring.com>
2021-04-12 18:39:14 -0500

    Added input and output management

--- Client.java
+++ Client.java
@@ -1,14 +1,20 @@
+import java.util.*;
+
 class Client
 {
     Socket socket;
     InetAddress IP;
     int PORT;
     String username;
+    BufferedReader output;
+    PrintWriter input;
 
     Client(Socket socket)
     {
         this.socket = socket;
         PORT = socket.getPort();
         IP = socket.getLocalAddress();
+        input = new PrintWriter(this.socket.getOutputStream());
+        output = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
     }
 }
